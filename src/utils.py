483b7ed149a974e52146e1354b908aa84b139eac
Saber GRAF <saber.graf@neuroservices-alliance.com>
2024-12-23 19:01:19 +0100

    add epilepsy notebook

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import torch
 import numpy as np
-import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from augmentation import scaling, jitter, permutation
